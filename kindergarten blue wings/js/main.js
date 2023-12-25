bc4924b29537aec4311592dbe4abb26bdc693eb3
munakhwaireh <137753449+munakhwaireh@users.noreply.github.com>
2023-12-25 16:59:59 +0200

    lib

--- kindergarten blue wings/js/main.js
+++ kindergarten blue wings/js/main.js
@@ -77,9 +77,73 @@
         }
     });
 
-
-
-
-
 })(jQuery);
 
+//
+// // Your existing JavaScript code...
+//
+// // Additional JavaScript for Library page
+// document.addEventListener('DOMContentLoaded', function () {
+//     // Array to store books (replace this with your backend/database logic)
+//     const books = [];
+//
+//     // Function to add a book
+//     window.addBook = function () {
+//         const bookCover = document.getElementById('bookCover').value;
+//         const bookName = document.getElementById('bookName').value;
+//         const suitableAge = document.getElementById('suitableAge').value;
+//         const bookType = document.getElementById('bookType').value;
+//
+//         // Validate input (you can add more validation logic)
+//         if (!bookCover || !bookName || !suitableAge || !bookType) {
+//             alert('Please fill in all fields.');
+//             return;
+//         }
+//
+//         // Create a new book object
+//         const newBook = {
+//             cover: bookCover,
+//             name: bookName,
+//             age: suitableAge,
+//             type: bookType,
+//         };
+//
+//         // Add the book to the array
+//         books.push(newBook);
+//
+//         // Clear the form fields
+//         document.getElementById('bookCover').value = '';
+//         document.getElementById('bookName').value = '';
+//         document.getElementById('suitableAge').value = '';
+//         document.getElementById('bookType').value = '';
+//
+//         // Refresh the book list
+//         refreshBookList();
+//     };
+//
+//     // Function to refresh the book list
+//     function refreshBookList() {
+//         const bookListContainer = document.getElementById('bookList');
+//
+//         // Clear the existing content
+//         bookListContainer.innerHTML = '';
+//
+//         // Generate the book list HTML
+//         const bookListHTML = books.map(book => `
+//             <div class="card mb-3">
+//                 <img src="${book.cover}" class="card-img-top" alt="${book.name}">
+//                 <div class="card-body">
+//                     <h5 class="card-title">${book.name}</h5>
+//                     <p class="card-text">Suitable Age: ${book.age}</p>
+//                     <p class="card-text">Type: ${book.type}</p>
+//                 </div>
+//             </div>
+//         `).join('');
+//
+//         // Append the HTML to the container
+//         bookListContainer.innerHTML = bookListHTML;
+//     }
+//
+//     // Call the initial refresh
+//     refreshBookList();
+// });
